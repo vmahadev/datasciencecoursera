387b3e2d31bfa2f154b22e5f7fabe8b788e17d8d
V Mahadev <v.mahadev@gmail.com>
2014-08-11 07:07:31 +0530

    Recent from the machine

--- Exploratory/Scripts/plot4.R
+++ Exploratory/Scripts/plot4.R
@@ -16,7 +16,7 @@ LoadData<-function()
   plot(DTHPCC$DTime,DTHPCC$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering",col="black")
   lines(xy.coords(DTHPCC$DTime,DTHPCC$Sub_metering_2),type="l",col="red")
   lines(DTHPCC$DTime,DTHPCC$Sub_metering_3,type="l",col="blue")
-  legend("topright",lty=1,col=c("black","red","blue"),
+  legend("topright",lty=1,col=c("black","red","blue"),cex=0.6,           
          legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
   )
   plot(DTHPCC$DTime,DTHPCC$Global_reactive_power,type="l",col="black",xlab="datetime",ylab="Global_reactive_power")
